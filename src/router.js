52ce03ddfbb28afac49e70ff5c3cb6fd25dfc512
TrovilhoJosh <78884647+TrovilhoJosh@users.noreply.github.com>
2023-05-30 15:54:59 -0300

    Criando pasta backend

--- src/router.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const tasksController = require('./controllers/tasksController');
-
-const router = express.Router();
-
-router.get('/tasks', tasksController.getAll);
-
-module.exports = router;
